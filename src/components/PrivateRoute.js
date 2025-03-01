3b4a8c80d3fa2683bbae93398d9dd2c1257ff231
Shahinsha A S <shahinsha.as@zohocorp.com>
2025-03-01 18:52:15 +0530

    Fix for reidrection

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,12 +2,20 @@ import { useSelector } from 'react-redux';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { getLocalStorageData } from '../utils/utils';
 
+const allowedPaths = ['/dashboard', '/reports/'];
+
 const PrivateRoute = () => {
-    const isLoggedIn = getLocalStorageData('userObj','{}');
+    const isLoggedIn = getLocalStorageData('userObj', '{}');
     const location = useLocation();
 
+    const isPathAllowed = allowedPaths.some(path=>location.pathname.includes(path));
+
     if (isLoggedIn.userName) {
-        return <Outlet />; // Render the child component (nested route)
+        if (isPathAllowed) {
+            return <Outlet />; // Render the child component (nested route)
+        } else {
+            return <Navigate to="/dashboard" replace />; // Redirect to /dashboard if path is not allowed
+        }
     } else {
         return <Navigate to="/login" state={{ from: location }} replace />; // Redirect to login if not logged in
     }
